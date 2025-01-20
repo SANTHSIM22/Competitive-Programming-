f2e736775f1a7a568a28daaee9c5e8fef417ac97
SANTHSIM22 <santhsimviriledsouza123456@gmail.com>
2025-01-20 15:42:10 +0530

    Digit summations, Conditions,Multiples

---
+++ codeforces/F_Digits_Summation.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long int a,b;
+    cin>>a;
+    cin>>b;
+    long long int d= a%10;
+    long long int c=b%10;
+    cout<<c+d;
+}
